5745f974534aa7fa7854fdb1e95fc30f27794a54
Brendan Le <80903766+brendanle65@users.noreply.github.com>
2025-08-30 19:43:00 -0600

    initial setup

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  let count = $state(0);
+</script>
+
+<main>
+  <h1>Hello World</h1>
+  <button onclick={() => count++}>
+    clicks: {count}
+  </button>
+</main>
+
+<style>
+  h1 {
+    color: red;
+  }
+</style>
